18ef0f6455d1f37530af11d861aca3e25c6b6841
vl0011 <vl0011@naver.com>
2023-06-04 16:18:43 +0900

    EDIT extern c

--- components/grepfa_uuid/include/grepfa_uuid.h
+++ components/grepfa_uuid/include/grepfa_uuid.h
@@ -5,8 +5,20 @@
 #ifndef P1_GREPFA_UUID_H
 #define P1_GREPFA_UUID_H
 
+#define UUID_STR_LEN 37
+
+#include <cstdint>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void random_uuid(char* desc);
 void random_u128(uint64_t* word);
 void bin_to_uuid(uint64_t* word, char* desc);
+
+#ifdef __cplusplus
+};
+#endif
 
 #endif //P1_GREPFA_UUID_H
